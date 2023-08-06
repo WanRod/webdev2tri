cc34863f97e21ceec6dc9f9bbad8f03157604b2a
MonaMagis <nafaster918@gmai.com>
2023-08-05 23:05:05 -0300

    Question Three 1: Completed

---
+++ questions/questionthree.1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Question 3</title>
+</head>
+
+<body>
+    <header>
+        <h2 class="header">Web Development</h2>
+    </header>
+
+    <main>
+        <h2 class="main">Assignment: Question 3</h2>
+
+        <form action="questionthree.2.php" method="post">
+
+            <p class="question">Question 1 - Qual a capital da França?</p>
+
+            <ul>
+                <p><span>A)</span> <a href="questionthree.2.php?value1=1">Marselha</a></p>
+                <p><span>B)</span> <a href="questionthree.2.php?value1=2">Lyon</a></p>
+                <p><span>C)</span> <a href="questionthree.2.php?value1=3">Nice</a></p>
+                <p><span>D)</span> <a href="questionthree.2.php?value1=4" class="correctanswer">Paris</a></p>
+                <p><span>E)</span> <a href="questionthree.2.php?value1=5">Annercy</a></p>
+            </ul>
+
+        </form>
+
+        <a href="questionthree.1.php">Cancel</a><br><br>
+        <a href="../index.php">Home page</a>
+
+    </main>
+
+    <footer>
+        <p class="footer"> Mariana e Wanderson - &copy; 2023</p>
+
+    </footer>
+</body>
+
+</html>
